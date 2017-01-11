d0026b6cf42460b383c19bc674d0f27bfd3f1d75
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-01-11 17:21:55 +0200

    Added items data

---
+++ js/items.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const ITEMS = [
+    {
+      id: 1,
+        name: 'Only Skinny Jeans',
+        price: 65.50,
+        source: '1.jpg',
+        status: '',
+        thumbnails: ['1.jpg', '2.2.jpg']
+   },
+
+    {
+      id: 2,
+        name: 'Neck Knitted Jumper',
+        price: 76.25,
+        source: '1.jpeg',
+        status: '',
+        thumbnails: ['1.jpeg', '1.2.jpg', '1.3.jpeg']
+   }
+]
