43dac195356bc533c83f35bf17d303819e1a5993
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-01-17 22:31:59 +0200

    Added new properties to items objects

--- js/items.js
+++ js/items.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const ITEMS = [
+
     {
       id: 1,
         name: 'Only Skinny Jeans',
         price: 65.50,
         source: '1.jpg',
-        status: '',
-        thumbnails: ['1.jpg', '2.2.jpg']
+        thumbnails: ['1.jpg', '2.2.jpg'],
+        quantity: 1,
+        color: '',
+        size: ''
    },
 
     {
@@ -15,7 +18,9 @@ const ITEMS = [
         name: 'Neck Knitted Jumper',
         price: 76.25,
         source: '1.jpeg',
-        status: '',
-        thumbnails: ['1.jpeg', '1.2.jpg', '1.3.jpeg']
+        thumbnails: ['1.jpeg', '1.2.jpg', '1.3.jpeg'],
+        quantity: 1,
+        color: '',
+        size: ''
    }
 ]
